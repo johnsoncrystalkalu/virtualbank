4c417ae584082b2fc5bcec549e65ca07d55f4c32
Treasure <treasureihedigbo1@gmail.com>
2020-09-09 16:22:33 +0100

    trying to set what the balance should show

---
+++ main.js
@@ -0,0 +1,17 @@
+var add;
+
+
+function addMoney (){
+    add = window.prompt("Enter amount to add");
+    alert("You just added N" + add + " to your account");
+}
+
+function withdrawMoney (){
+    var withdraw = window.prompt("Enter amount to withdraw");
+}
+
+function checkBalance (){
+    var defaultBalance = 0;
+    alert("You have N" +add + defaultBalance+ " in your account.\n Add some money and begin saving.");
+   
+}
