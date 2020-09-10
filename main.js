d34c336664092525df6c3fe884e9fd4fe0e2f0dd
Treasure <treasureihedigbo1@gmail.com>
2020-09-10 11:29:54 +0100

    cant seem to figure out how to deal with the balance

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 var add;
+var balance;
+balance = 0;
+balance += add;
+var withdraw;
 
 
 function addMoney (){
     add = window.prompt("Enter amount to add");
-    alert("You just added N" + add + " to your account");
-}
+    alert("You have just added N" + add + " to your account.");
+ }
+    
 
-function withdrawMoney (){
-    var withdraw = window.prompt("Enter amount to withdraw");
-}
+ function withdrawMoney (){
+     withdraw = window.prompt("Enter amount to withdraw");
+    alert("You have just withdrawn N" + withdraw + " from your account.");
+ }
 
-function checkBalance (){
-    var defaultBalance = 0;
-    alert("You have N" +add + defaultBalance+ " in your account.\n Add some money and begin saving.");
+ function checkBalance (){
+     alert("You have N" + balance + "left in your account");
    
-}
+ }
